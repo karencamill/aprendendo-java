15df17d513e89d45a5d5475e8e7625e585b11362
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-15 22:57:00 -0300

    Add files via upload
    Elaboração de um programa que realiza a multiplicação de dois valores numéricos do tipo de dado real digitados pelo usuário.

---
+++ Exe002/src/exe002/Exe002.java
@@ -0,0 +1,27 @@
+package exe002;
+
+import java.util.Scanner;
+
+public class Exe002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o primeiro número: ");
+		double num1 = teclado.nextInt();
+		System.out.println("O primeiro número digitado foi " + num1);
+		
+		System.out.println("Digite o segundo número: ");
+		double num2 = teclado.nextInt();
+		System.out.println("O segundo número digitado foi " + num2);
+		
+		double multiplicacao = num1 * num2;
+		System.out.println("A multiplicação entre " + num1 + " e " + num2+ " é igual a : " + multiplicacao);
+				
+		
+		
+
+	}
+
+}
