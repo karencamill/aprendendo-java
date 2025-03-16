eb49dba2b74b77d10cc88a2b499633586bf9f8d3
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-15 21:36:39 -0300

    Add files via upload

---
+++ Aula04/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Se eu não desistir, o que será que acontece?");
+	}
+
+}
