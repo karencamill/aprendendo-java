bb816de88752ba3d531ec7d4d9c17012d985e40c
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-18 09:22:33 -0300

    Update OperadoresAritmeticos.java

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -5,11 +5,26 @@ public class OperadoresAritmeticos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int n1 = 3;
+		/*int n1 = 3;
 		int n2 = 5;
 		
 		float m = (n1 + n2) / 2;
-		System.out.println("A média é: " + m);
+		System.out.println("A média é: " + m);*/
+
+		/*int numero = 3;
+		int valor = 4 + numero--;
+		int resultado = numero;
+		
+		//System.out.println(valor);
+		System.out.println(resultado);*/
+		
+		int x = 4;
+		x += 2; // x = x + 2
+		System.out.println(x);
+		
+		int y = 9;
+		y *= 3; // x = x * 3
+		System.out.println(y);
 		
 	}
 
