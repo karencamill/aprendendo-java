fc393db77361d8254b1f2e16decd856558796a78
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-18 09:01:36 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,16 @@
+package operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n1 = 3;
+		int n2 = 5;
+		
+		float m = (n1 + n2) / 2;
+		System.out.println("A média é: " + m);
+		
+	}
+
+}
