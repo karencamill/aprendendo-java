32b9c619d84cc29515c7d5cc75e37ac6e9d24977
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-19 15:06:23 -0300

    Add files via upload

---
+++ OperadorLogico/src/logico/OperadorLogico.java
@@ -0,0 +1,21 @@
+package logico;
+
+public class OperadorLogico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x, y, z;
+		x = 4;
+		y = 6;
+		z = 3;
+		boolean resultado;
+		
+		//resultado = (x>y && y>z) ?true:false; && as duas expressões tem que ser V
+		//resultado = (x>y || y>z) ?true:false; || uma das expressões tem que ser V
+		resultado = (x>y ^ y==z) ?true:false;
+		System.out.println(resultado);
+		
+	}
+
+}
