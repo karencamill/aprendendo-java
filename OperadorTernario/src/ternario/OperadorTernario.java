71d98795555941fd7c033458d9c3f3b70e253e32
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-19 14:40:02 -0300

    Update OperadorTernario.java

--- OperadorTernario/src/ternario/OperadorTernario.java
+++ OperadorTernario/src/ternario/OperadorTernario.java
@@ -4,11 +4,22 @@ public class OperadorTernario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n1, n2, r;
+		/*int n1, n2, r;
 		n1 = 9;
 		n2 = 8;
 		r = (n1>n2) ?0:1; //Se a condição for verdadeira executa depois do ? se não executa depois dos :
-		System.out.println(r);		
+		System.out.println(r);*/
+
+		String nome1 = "Karen";
+		String nome2 = "Karen";
+		String nome3 = new String("Karen"); // Não é igual, o conteúdo que é igual.
+		String resultado;
+		
+		//resultado = (nome1 == nome2) ?"igual" : "diferente";
+		//System.out.println(resultado);
+		
+		resultado = (nome1.equals(nome3)) ? "igual" : "diferente"; //equals = método que verifica se o conteúdo de um método é igual ao outro
+		System.out.println(resultado);
 	}
 
 }
