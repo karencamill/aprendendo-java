6ab64ca38cd1cffbe056f5828ae5b34ece0d7535
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-19 14:28:21 -0300

    Add files via upload

---
+++ OperadorTernario/src/ternario/OperadorTernario.java
@@ -0,0 +1,14 @@
+package ternario;
+
+public class OperadorTernario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1, n2, r;
+		n1 = 9;
+		n2 = 8;
+		r = (n1>n2) ?0:1; //Se a condição for verdadeira executa depois do ? se não executa depois dos :
+		System.out.println(r);		
+	}
+
+}
