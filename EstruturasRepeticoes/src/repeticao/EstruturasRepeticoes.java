9f414fc05d54247f0961dfd7b7c652cd283a6123
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-23 20:43:50 -0300

    Add files via upload

---
+++ EstruturasRepeticoes/src/repeticao/EstruturasRepeticoes.java
@@ -0,0 +1,18 @@
+package repeticao;
+
+public class EstruturasRepeticoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			int cc = 1;
+			while (cc <= 10) {
+				
+				System.out.println("Cambalhota" + cc);	
+				cc++;
+			}
+				
+	     }
+	}
+
+
