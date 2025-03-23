ab9d01c3553629a0dbc43d8da9211c910eebd89c
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-23 16:05:44 -0300

    Update EstruturasCondicionais.java

--- EstruturasCondicionais/src/condicionais/EstruturasCondicionais.java
+++ EstruturasCondicionais/src/condicionais/EstruturasCondicionais.java
@@ -57,7 +57,35 @@ public class EstruturasCondicionais {
 		}else {
 			System.out.println("O número é ÍMPAR");
 		}
+	
+		System.out.printl("QUARTO EXEMPLO"); // USANDO SWITCH
 
+		System.out.println("Quantas pernas: ");
+		int perna = teclado.nextInt();
+		String tipo;
 		
+		System.out.print("Isso é um(a): ");
+		switch(perna) {
+			case 1:
+				tipo = "Saci";
+				break;
+				
+			case 2:
+				tipo = "Bípede";
+				break;
+			case 3: 
+				tipo = "Tripé";
+				break;
+			case 4:
+				tipo = "Quadrupéde";
+				break;
+			case 6:
+				tipo = "Aranha";
+				break;
+			default:
+				tipo = "ET";
+		}		
+		System.out.println(tipo);
+
 	}
 }
