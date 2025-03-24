0429e538033450171cf46ef0d5af28ce2d14f807
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-23 21:02:58 -0300

    Update EstruturasRepeticoes.java

--- EstruturasRepeticoes/src/repeticao/EstruturasRepeticoes.java
+++ EstruturasRepeticoes/src/repeticao/EstruturasRepeticoes.java
@@ -6,12 +6,27 @@ public class EstruturasRepeticoes {
 		// TODO Auto-generated method stub
 	
 			int cc = 1;
-			while (cc <= 10) {
-				
+			while (cc <= 10) {	
 				System.out.println("Cambalhota" + cc);	
 				cc++;
 			}
-				
+
+		// EXEMPLO 2
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Vamos calcular o fatorial");
+		System.out.println("Digite um número: ");
+		int n = teclado.nextInt();
+		
+		int f = 1;
+		int c = n;
+		
+		while (c>=1) {
+			f *= c;
+			c--;
+		}		
+		
+		System.out.print(f);
+					
 	     }
 	}
 
