df4c514dbf6efd38b95c3628f70efe84b2c4ad32
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-24 13:51:25 -0300

    Add files via upload

---
+++ Vetores/src/vetor/Vetores.java
@@ -0,0 +1,13 @@
+package vetor;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n []= {3, 4, 8, 9, 6, 2};
+		System.out.println("O número de casas é " + n.length);
+		for (int c=0; c<=5; c++){
+			System.out.println("Na posição " + c + " temos o valor " + n[c]);
+		}
+	}
+}
