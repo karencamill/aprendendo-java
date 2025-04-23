52a84f722468830dbcf23e5aebf091aa475c325d
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-04-23 00:08:27 -0300

    Update EstruturasRepeticoes.java

--- EstruturasRepeticoes/src/repeticao/EstruturasRepeticoes.java
+++ EstruturasRepeticoes/src/repeticao/EstruturasRepeticoes.java
@@ -4,10 +4,11 @@ public class EstruturasRepeticoes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-			int cc = 1;
-			while (cc <= 10) {	
-				System.out.println("Cambalhota" + cc);	
+
+		//EXEMPLO 1
+		int cc = 1;
+		while (cc <= 10) {	
+			System.out.println("Cambalhota" + cc);	
 				cc++;
 			}
 
